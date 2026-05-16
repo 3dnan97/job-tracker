9e6655ed8a7f486a426ae4b82a2c3d6c1724c5d4
Adnan Al-khlaki <Adnanalkhlaki@gmail.com>
2026-05-16 04:07:31 +0200

    Refactor Layout and Nav components for improved structure and navigation styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router-dom"
 import Nav from "./Nav"
 export default function Layout(){
     return(
-        <>
-            <Nav />
-            <Outlet />
-        </>
+        <div className="flex flex-col md:items-center md:justify-center min-h-screen">
+            <div className="md:w-[1000px] md:border border-brand-border rounded-20px overflow-hidden">
+                <Nav />
+                <Outlet />
+            </div>
+        </div>
     )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,40 +1,58 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import {STRINGS} from '../i18n/strings'
 import useAuth from '../hooks/useAuth'
-export default function Nav(){
+export default function Nav(){   
     const {user, signOut} = useAuth()
     return(
-        <>
-            <nav className="flex justify-between items-center p-4 border-b border-gray-300 rounded-lg">
-                <div>{STRINGS.common.appName}</div>
+        <div className='md:py-4 md:px-8 md:bg-white md:border-b border-brand-border'>
+            <nav className="flex justify-between items-center p-4 md:p-0">
+                <Link to='/' className='flex items-center gap-[10px]'>
+                    <div className="h-6 w-6 rounded-lg bg-brand-blue"></div>
+                    <div className='text-[18px] font-bold'>{STRINGS.common.appName}</div>
+                </Link>
                 <div className="flex gap-6 items-center">
-                    <ul className="hidden md:flex gap-4">
+                    <ul className="hidden md:flex gap-6 font-semibold text-sm text-brand-slate">
                         <li>
-                            <Link to="/" className="hover:text-black cursor-pointer">{STRINGS.nav.dashboard}</Link>
+                            <NavLink 
+                                to="/" 
+                                className={({isActive}) => `${isActive ? 'text-brand-dark cursor-default' : 'hover:text-brand-blue duration-200 cursor-pointer'}`}>
+                                    {STRINGS.nav.dashboard}
+                            </NavLink>
                         </li>
                         <li>
-                            <Link to="/jobs" className="hover:text-black cursor-pointer">{STRINGS.nav.jobs}</Link>
+                            <NavLink 
+                                to="/jobs" 
+                                className={({isActive}) => `${isActive ? 'text-brand-dark cursor-default' : 'hover:text-brand-blue duration-200 cursor-pointer'}`}>
+                                    {STRINGS.nav.jobs}
+                            </NavLink>
                         </li>
-                        <li>
+                        <li className='cursor-default'>
                             {user?.user_metadata.name}
                         </li>
                     </ul>
                     <button 
                         onClick={signOut}
-                        className="py-1 px-2 border border-gray-300 rounded-lg hover:bg-blue-900 hover:text-white cursor-pointer">
+                        className="py-1 px-2 rounded-lg font-semibold text-[13px] hover:text-white border border-brand-border hover:bg-brand-blue duration-200 cursor-pointer">
                         {STRINGS.nav.logout}
                     </button>
                 </div>
             </nav>
-            <ul className="md:hidden flex gap-4 mt-4 px-4">
-                
+            <ul className="flex gap-2 px-4 pb-3 md:hidden">
                 <li>
-                    <Link to="/">{STRINGS.nav.dashboard}</Link>
+                    <NavLink 
+                        to="/" 
+                        className={({isActive}) =>`px-3 py-2 rounded-full text-[13px] font-semibold ${isActive ? 'bg-brand-blue text-white' : 'bg-brand-border'}`}>
+                            {STRINGS.nav.dashboard}
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/jobs">{STRINGS.nav.jobs}</Link>
+                    <NavLink 
+                        to="/jobs" 
+                        className={({isActive}) =>`px-3 py-2 rounded-full text-[13px] font-semibold ${isActive ? 'bg-brand-blue text-white' : 'bg-brand-border'}`}>
+                            {STRINGS.nav.jobs}
+                    </NavLink>
                 </li>
             </ul>
-        </>
+        </div>
     )
 }
